16441feae588cf8acdd8ac33361112d38d93f6bf
neuralgya <neuralgya>
2003-02-14 17:58:00 +0000

    - Estructura básica neuralgya

--- worldspace/w3dsrc/audio/audio.c
+++ worldspace/w3dsrc/audio/audio.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "audio.h"
+#include "w3d_base.h"
+#include "globales.h"
+#ifdef _LINUX
+#include <AL/al.h>
+#include <AL/alc.h>
+#include <AL/alut.h>
+#include <AL/alext.h>
+#endif
+#ifdef _WIN32
+#include <al\al.h>
+#include <al\alc.h>
+#include <al\alut.h>
+#include <eax.h>
+#endif
 
 /*!
 ================================== DECLARACION DE VARIABLES
@@ -10,64 +24,107 @@ ALCdevice *Device = NULL;       /* Dispositivo de sonido */
 #ifdef _LINUX
     ALvoid * Context = NULL;    /* Contexto para Linux */
 
-#endif  /*  */
+#endif  /* 
+ */
 #ifdef _WIN32
     ALCcontext * Context = NULL;        /* Contexto para Windows */
 
 #endif
 int inicializar_audio()
 {
-    
+    
+
         /* Asignamos el mejor dispositivo de audio disponible */ 
 #ifdef _LINUX
-        if ((Device = alcOpenDevice((ALubyte *) "alsa")) == NULL) {
-        log_msj("No existe ALSA Backend\n");
-        if ((Device = alcOpenDevice((ALubyte *) "emu10k1")) == NULL) {
-            log_msj("No existe Sound Blaster Live Backend\n");
-            if ((Device = alcOpenDevice((ALubyte *) "waveout")) == NULL) {
-                log_msj("No existe waveout Backend\n");
-                if ((Device = alcOpenDevice(NULL)) == NULL) {
-                    log_msj
+        
+if ((Device = alcOpenDevice((ALubyte *) "alsa")) == NULL) {
+        
+log_msj("No existe ALSA Backend\n");
+        
+if ((Device = alcOpenDevice((ALubyte *) "emu10k1")) == NULL) {
+            
+log_msj("No existe Sound Blaster Live Backend\n");
+            
+if ((Device = alcOpenDevice((ALubyte *) "waveout")) == NULL) {
+                
+log_msj("No existe waveout Backend\n");
+                
+if ((Device = alcOpenDevice(NULL)) == NULL) {
+                    
+log_msj
                         ("No hay disponible ningun dispositivo de audio\n");
-                    return NO;
-                }
-            }
-        }
-    }
+                    
+return NO;
+                
+}
+            
+}
+        
+}
     
-#endif  /*  */
+}
+    
+#endif  /* 
+ */
 #ifdef _WIN32
         if ((Device = alcOpenDevice((ALubyte *) "DirectSound3D")) == NULL) {
-        log_msj("No existe DirectSound3D Backend\n");
-        if ((Device = alcOpenDevice((ALubyte *) "DirectSound")) == NULL) {
-            log_msj("No existe DirectSound Backend\n");
-            if ((Device = alcOpenDevice((ALubyte *) "WaveOut")) == NULL) {
-                log_msj("No existe WaveOut Backend\n");
-                if ((Device = alcOpenDevice(NULL)) == NULL) {
-                    log_msj
+        
+log_msj("No existe DirectSound3D Backend\n");
+        
+if ((Device = alcOpenDevice((ALubyte *) "DirectSound")) == NULL) {
+            
+log_msj("No existe DirectSound Backend\n");
+            
+if ((Device = alcOpenDevice((ALubyte *) "WaveOut")) == NULL) {
+                
+log_msj("No existe WaveOut Backend\n");
+                
+if ((Device = alcOpenDevice(NULL)) == NULL) {
+                    
+log_msj
                         ("No hay disponible ningun dispositivo de audio\n");
-                    return NO;
-                }
-            }
-        }
-    }
+                    
+return NO;
+                
+}
+            
+}
+        
+}
     
-#endif  /*  */
+}
+    
+#endif  /* 
+ */
         
         /* Creamos un contexto y lo asignamos (comprobando si hay errores) */ 
         if (Device != NULL) {
-        Context = alcCreateContext(Device, NULL);      /* Creamos */
-        if (alcGetError(Device) != ALC_NO_ERROR) {
-            log_msj
+        
+Context = alcCreateContext(Device, NULL);      /* Creamos */
+        
+if (alcGetError(Device) != ALC_NO_ERROR) {
+            
+log_msj
                 ("No se puede crear un contexto para el sistema de audio\n");
-            return NO;
-        }
-        alcMakeContextCurrent(Context);       /* Asignamos */
-        if (alcGetError(Device) != ALC_NO_ERROR) {
-            log_msj
+            
+return NO;
+        
+}
+        
+
+alcMakeContextCurrent(Context);       /* Asignamos */
+        
+if (alcGetError(Device) != ALC_NO_ERROR) {
+            
+log_msj
                 ("No se puede asignar el contexto para el sistema de audio\n");
-            return NO;
-        }
-    }
-    return SI;
-}
+            
+return NO;
+        
+}
+    
+}
+    
+return SI;
+
+}
