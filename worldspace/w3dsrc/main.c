52a2dd9553c8ba6dccb9ad188ca86423b447e3b9
eric <eric>
2003-04-30 00:26:55 +0000

    - Modificado para pruebas de carga de texturas y uso de fuentes. ¡¡ Funciona !! Y me he rendido a poner el WinMain. No me dejaba modificar con Ming32, me pedía el WinMain,  que esperaba que cogiese de SDLmain. Con VC estaba funcionando. Así que he introducido con opciones del preprocesador el 'WinMain'. Por cierto, con Ming32 funciona la música, pero no con VC. Parece estable, pero habrá que vigilar que punteros deja abiertos. Para ello tendríamos que dar memoria solo con las funciones de 'memoria.c' y luego establecer un control de que se queda abierto.

--- worldspace/w3dsrc/main.c
+++ worldspace/w3dsrc/main.c
@@ -20,35 +20,83 @@
 #include "display.h"
 #include "engine.h"
 #include "audio.h"
+#include "textura.h"
+#include "globales.h"
 
 #ifdef _MSVC
 #include "sdl_gl.h"
 #endif // _MSVC
 
 /*!
-==================================================================
+=============================================================================
 Inicio de la aplicación. Punto de entrada.
-==================================================================
+=============================================================================
+  Modifico el main para probar que la carga de las texturas funciona
+  correctamente.
+=============================================================================
+  Generalmente los sistemas identifican que todo ha ido bien en un programa
+  si devuelve 0. Si devuelve distinto de cero, alguna incidencia ha ocurrido.
+  Voy a establecer que si hay algun error devuelvo 1, y si todo ha ido bien
+  el programa devolverá 0.
+  Y no  SI=1  y  NO=0.
+=============================================================================
+  Y al final me he tenido que rendir y meter las líneas de WinMain para
+  el compilador Ming32.
+  El caso es que para VC estaba funcionando lo del SDLmain,incluso también
+  para Ming32, pero cuando quería ampliar unas pequeñas modificaciones
+  no me dejaba.
+=============================================================================
 !*/
-
-int main(int iArg, char **vArg)
+#ifdef _LINUX
+int main(int iArg, char *vArg[])
 {
+#endif  // _LINUX
+//==================================================================
+#ifdef _WIN32
+#ifndef _MSVC
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int nCmdShow)
+{
+	int     iArg = sizeof(lpCmdLine)/sizeof(LPSTR);
+	char ** vArg = (char **) lpCmdLine;
+#else
+//==================================================================
+int main(int iArg, char *vArg[])
+{
+#endif  // _MSVC
+#endif  // _WIN32
+//==================================================================
+	int iError = 0;
 
-    if (!ini_bios(iArg, vArg))
-        return NO;
+    if (ini_bios(iArg, vArg))
+	{
+        if (ini_display())
+		{
+            if (ini_audio())
+            {
+	            if (
+                       ( carga_listaFuentes(&vListaFuentes)   )
+                  /*|| ( carga_listaTexturas(&vListaTexturas) ) Lista de texturas */
+                  /*|| ( carga_listaModelos(&vListaModelos)   ) Lista de modelos  Las texturas deben de pasarse antes */
+                    || ( genera_texturas()                    )
+                   )
+	                iError = 1;
+				else
+				{
+	                if (ini_gl(SI))           ;  // Bucle principal
+					else            iError = 1;
+				}
+	            cerrar_texturas();
+                cerrar_audio();
+			}
+			else iError = 1;
+            cerrar_display();
+		}
+		else iError = 1;
+        cerrar_bios();
+	}
+	else iError = 1;
 
-    if (!ini_display())
-        return NO;
-
-    if (!ini_audio())
-        return NO;
-
-    if (!ini_gl(SI))
-        return NO;
-
-    cerrar_audio();
-    cerrar_display();
-    cerrar_bios();
-
-    return SI;
+    return iError;
 }
