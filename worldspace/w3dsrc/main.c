2b29bbb699a853663b19b879cb072551d88d9307
eric <eric>
2003-05-15 20:09:26 +0000

    - Mejora del WinMain, construcción de parametros, para MingW32.

--- worldspace/w3dsrc/main.c
+++ worldspace/w3dsrc/main.c
@@ -16,6 +16,7 @@
  ***************************************************************************/
 
 #include "w3d_base.h"
+#include "memoria.h"
 #include "bios.h"
 #include "display.h"
 #include "engine.h"
@@ -23,50 +24,40 @@
 #include "textura.h"
 #include "globales.h"
 
-#ifdef _MSVC
-#include "sdl_gl.h"
-#endif // _MSVC
+//===========================================================================
+//  Inicio de la aplicación. Punto de entrada.
+//===========================================================================
+//  Modifico el main para probar que la carga de las texturas funciona
+//  correctamente.
+//===========================================================================
+//  Generalmente los sistemas identifican que todo ha ido bien en un programa
+//  si devuelve 0. Si devuelve distinto de cero, alguna incidencia ha
+//  ocurrido.
+//  Voy a establecer que si hay algun error devuelvo 1, y si todo ha ido bien
+//  el programa devolverá 0.
+//  Y no  SI=1  y  NO=0.
+//===========================================================================
+//  Y al final me he tenido que rendir y meter las líneas de WinMain para
+//  el compilador Ming32.
+//  El caso es que para VC está funcionando lo del SDLmain, incluso también
+//  para MingW32 algunas veces, pero cuando quería ampliar unas pequeñas
+//  modificaciones el MingW32 cascaba pidiendo la funcion WinMain.
+//===========================================================================
 
-/*!
-=============================================================================
-Inicio de la aplicación. Punto de entrada.
-=============================================================================
-  Modifico el main para probar que la carga de las texturas funciona
-  correctamente.
-=============================================================================
-  Generalmente los sistemas identifican que todo ha ido bien en un programa
-  si devuelve 0. Si devuelve distinto de cero, alguna incidencia ha ocurrido.
-  Voy a establecer que si hay algun error devuelvo 1, y si todo ha ido bien
-  el programa devolverá 0.
-  Y no  SI=1  y  NO=0.
-=============================================================================
-  Y al final me he tenido que rendir y meter las líneas de WinMain para
-  el compilador Ming32.
-  El caso es que para VC estaba funcionando lo del SDLmain,incluso también
-  para Ming32, pero cuando quería ampliar unas pequeñas modificaciones
-  no me dejaba.
-=============================================================================
-!*/
-#ifdef _LINUX
+
+//===========================================================================
+#if !(defined(_WIN32) && defined(_MSVC)) && !(defined(_LINUX))
+#include "mimain.h"
+#endif // !(defined(_WIN32) && defined(_MSVC)) && !(defined(_LINUX))
+//===========================================================================
+//  Añadimos una construcción de argumentos con WinMain, ya que lo que
+//  pusimos fue para salir del paso.
+//  Y todo ello lo desplazamos a un fichero de cabecera para que el 'main'
+//  quede limpio y legible.
+//===========================================================================
+
 int main(int iArg, char *vArg[])
 {
-#endif  // _LINUX
-//==================================================================
-#ifdef _WIN32
-#ifndef _MSVC
-int WINAPI WinMain(HINSTANCE hInstance,
-                   HINSTANCE hPrevInstance,
-                   LPSTR lpCmdLine, int nCmdShow)
-{
-	int     iArg = sizeof(lpCmdLine)/sizeof(LPSTR);
-	char ** vArg = (char **) lpCmdLine;
-#else
-//==================================================================
-int main(int iArg, char *vArg[])
-{
-#endif  // _MSVC
-#endif  // _WIN32
-//==================================================================
 	int iError = 0;
 	int iAudio_OK= NO;
 
@@ -76,30 +67,34 @@ int main(int iArg, char *vArg[])
 		{
             iAudio_OK=ini_audio();
 
-	            if (
-                       ( carga_listaFuentes(&vListaFuentes)   )
-                  /*|| ( carga_listaTexturas(&vListaTexturas) ) Lista de texturas */
-                  /*|| ( carga_listaModelos(&vListaModelos)   ) Lista de modelos  Las texturas deben de pasarse antes */
-                    || ( genera_texturas()                    )
-                   )
-	                iError = 1;
-				else
-				{
-	                if (ini_gl(SI))           ;  // Bucle principal
-					else            iError = 1;
-				}
-	            cerrar_texturas();
-                if (iAudio_OK)
-			cerrar_audio();
-
-
-
+	        if (
+                  ( carga_listaFuentes(&vListaFuentes)   )
+             /*|| ( carga_listaTexturas(&vListaTexturas) ) Lista de texturas */
+             /*|| ( carga_listaModelos(&vListaModelos)   ) Lista de modelos  Las texturas deben de pasarse antes */
+               || ( genera_texturas()                    )
+               )
+	            iError = 1;
+			else
+			{
+	            if (ini_gl(SI))           ;  // Bucle principal
+				else            iError = 1;
+			}
+            cerrar_texturas();
+            if (iAudio_OK)
+			    cerrar_audio();
             cerrar_display();
 		}
 		else iError = 1;
+
+		control_memoria ();
         cerrar_bios();
 	}
 	else iError = 1;
 
     return iError;
 }
+
+
+//===========================================================================
+//  Fin de main.c
+//===========================================================================
